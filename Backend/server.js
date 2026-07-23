fc8295edc3809e6bc80ecac8cbfd1f44e0322a72
arushijain18 <your-email@example.com>
2026-07-23 19:59:22 +0530

    fix cors middleware order

--- Backend/server.js
+++ Backend/server.js
@@ -10,28 +10,25 @@ import authRoutes from "./routes/auth.js";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.json());         
-
-app.use(passport.initialize());
-app.use("/api/auth", authRoutes);
-app.use("/api", chatRoutes);
 app.use(cors({
   origin: ["http://localhost:5173", "https://sigmagpt-frontend-53bq.onrender.com"],
   credentials: true
 }));
+app.use(express.json());         
+app.use(passport.initialize());
+app.use("/api/auth", authRoutes);
+app.use("/api", chatRoutes);
+
 const client = new OpenAI({
   apiKey: process.env.GROQ_API_KEY,
   baseURL: "https://api.groq.com/openai/v1",
 });
 
-
-// ALways connect to the server and then connect to the databasee to  show the data 
 app.listen(PORT, () => {
   console.log(`server running on ${PORT}`);
   connectDB();
 });
 
-// this is used to connect with the database to get the data from mongodb
 const connectDB = async()=>{
   try{
     await mongoose.connect(process.env.MONGODB_URI);
